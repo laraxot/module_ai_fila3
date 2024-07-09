8b7f1a2d190b2322171417444a5048adc9219c3e
marco76tv <marco.sottana@gmail.com>
2024-07-09 13:02:26 +0200

    .

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 return [
     'name' => 'AI',
     // 'icon' => 'heroicon-o-cog', // icon on dashboard
-    'icon' => 'fas-air-freshener',
+    // 'icon' => 'fas-air-freshener',
+    'icon' => 'icon-brain',
     'navigation_sort' => 1,
 ];
