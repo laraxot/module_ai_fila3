ad56c3442407737749dec6a68b56df84fd3f13c0
marco <marco.sottana@gmail.com>
2024-08-19 09:30:01 +0200

    up

--- Providers/AIServiceProvider.php
+++ Providers/AIServiceProvider.php
@@ -10,6 +10,7 @@ use Modules\Xot\Providers\XotBaseServiceProvider;
 class AIServiceProvider extends XotBaseServiceProvider
 {
     public string $module_name = 'ai'; // lower del nome
+
     protected string $module_dir = __DIR__;
 
     protected string $module_ns = __NAMESPACE__;
