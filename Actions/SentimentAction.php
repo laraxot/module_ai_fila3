885f5595e90747bb409ec2f7c73203d8ec46ce07
marco <marco.sottana@gmail.com>
2024-09-13 08:37:26 +0200

    🔧 (gitignore): add vendor/, build/, docs/cache/, and cache/ to gitignore ✨ (CompletionAction.php): add new CompletionAction class for handling completions using OpenAI ✨ (SentimentAction.php): add new SentimentAction class for handling sentiment analysis using Transformers ✨ (Completion.php): add new Completion page for handling completions and sentiment analysis forms ✨ (composer.json): add new autoload paths for factories and seeders, and require codewithkyrian/transformers 📝 (repositories.md): add list of repositories related to AI and ML tools for Laravel 📝 (tutorials.md): add list of tutorials and resources related to AI and ML in Laravel

---
+++ Actions/SentimentAction.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @see https://dev.to/robertobutti/machine-learning-with-php-5gb
+ */
+
+declare(strict_types=1);
+
+namespace Modules\AI\Actions;
+
+use function Codewithkyrian\Transformers\Pipelines\pipeline;
+
+// 002 importing the Transformers class
+use Codewithkyrian\Transformers\Transformers;
+// 003 importing the pipeline function
+use Spatie\QueueableAction\QueueableAction;
+
+class SentimentAction
+{
+    use QueueableAction;
+
+    public function execute(string $prompt)
+    {
+        // 004 initializing the Transformers class setting the cache directory for models
+        Transformers::setup()->setCacheDir('./../cache/models')->apply();
+
+        // 005 initializing a pipeline for sentiment-analysis
+        $pipe = pipeline('sentiment-analysis');
+        $prompt = 'The quality of tools in the PHP ecosystem has greatly improved in recent years';
+        $out = $pipe($prompt);
+        dddx($out);
+        // array:2 [▼
+        //    "label" => "POSITIVE"
+        //    "score" => 0.99965798854828
+        // ]
+        // "time" => 20.914520025253
+
+        return $out;
+    }
+}
