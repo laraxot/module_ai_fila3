61c0262c3d302990d2a5e206200fee8ea846e34b
marco <marco.sottana@gmail.com>
2024-10-07 09:39:56 +0200

    🐛 (completion.blade.php): resolve merge conflict markers in completion.blade.php file

--- Resources/views/filament/pages/completion.blade.php
+++ Resources/views/filament/pages/completion.blade.php
@@ -3,10 +3,14 @@
         {{ $this->completionForm }}
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         <x-filament-panels::form.actions
 =======
         <x-filament-panels::form.actions 
 >>>>>>> fd7d110 (up)
+=======
+        <x-filament-panels::form.actions
+>>>>>>> 07a3400 (up)
             :actions="$this->getCompletionFormActions()"
         />
 
